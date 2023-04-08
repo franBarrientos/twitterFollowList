51c58867d9a9db5e6ebbd86457181cc03f3e654f
Fran <francobarrientos@gmail.com>
2023-04-08 20:12:25 -0300

    test

---
+++ prebavite/src/App.jsx
@@ -0,0 +1,10 @@
+
+export default function App() {
+  return (<>
+    <div className='font-4xl bg-black  text-red-600 text-center'>HOLAAAAAAAAAAA MI AMORR</div>
+    <button  className="bg-green-300 text-2xl">1</button>
+    <button  className="bg-green-300 text-2xl">1</button>
+    <button  className="bg-green-300 text-2xl">1</button>
+  </>
+  )
+}
